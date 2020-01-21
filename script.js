046cf6db4057682774123839c3f1f2cf454fd000
Kelly Cox <thekellycox@gmail.com>
2020-01-21 01:37:44 -0500

    added digit buttons and temporary method for adding values

---
+++ script.js
@@ -0,0 +1,20 @@
+function nothingButNumbers () {
+document.querySelector('input').addEventListener('keypress', function (e) {
+    const allowedChars = '0123456789.';
+    function contains(stringValue, charValue) {
+        return stringValue.indexOf(charValue) > -1;
+    }
+    const invalidKey = e.key.length === 1 && !contains(allowedChars, e.key) 
+    || e.key === '.' && contains(e.target.value, '.');
+    invalidKey && e.preventDefault();
+});}
+
+let values = [];
+let result = 0;
+
+function sumFunction() {
+    values.push(parseFloat(document.querySelector('input').value));
+    result = values.reduce((a,b)=> a+b , 0);
+    document.querySelector('input').value = result;
+    
+}
