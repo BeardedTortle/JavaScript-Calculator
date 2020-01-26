ba4c182bc667e0cdf5da009f249bf3842d85f129
Kelly Cox <thekellycox@gmail.com>
2020-01-25 19:49:15 -0500

    refactored code to include display DIV instead of input screen.

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-function nothingButNumbers () {
-    document.querySelector('input').addEventListener('keypress', function (e) {
-        const allowedChars = '0123456789.';
-        function contains(stringValue, charValue) {
-            return stringValue.indexOf(charValue) > -1;
-        }
-        const invalidKey = e.key.length === 1 && !contains(allowedChars, e.key) 
-        || e.key === '.' && contains(e.target.value, '.');
-        invalidKey && e.preventDefault();
-});}
-
-let values = [];
-let result = 0;
-
-// function sumFunction() {
-//     values.push(parseFloat(document.querySelector('input').value));
-//     document.querySelector('input').value = 0;
-//     result = values.reduce((a,b)=> a+b , 0);
-//     document.querySelector('input').value = result;
-    
-// }
-
-
 document.querySelectorAll('.digits').forEach(item => {
     item.addEventListener('click', e => {
-        document.querySelector('input').value += item.innerHTML;
-    })
-})
+        if(item.innerHTML === '.'){
+            console.log('you pressed decimal')
+            document.getElementById('currentInput').innerHTML += item.innerHTML;
+            document.getElementById('decimal').innerHTML = '';
+        }
+        document.getElementById('currentInput').innerHTML += item.innerHTML;
+    });
+});
 
+function sumFunction() {
+     
+}
 
-nothingButNumbers();
+function subFunction() {
+
+}
+
+function multFunction() {
+
+}
+
+function divFunction() {
+
+}
+
+function equalsFunction() {
+
+}
