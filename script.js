531d70ffa1a2429ec1ac88bab4ab991df6e036e6
Kelly Cox <thekellycox@gmail.com>
2020-01-27 22:06:05 -0500

    refactored for the millionth time. Still need a way to compute result based on operand chosen

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-document.querySelectorAll('.digits').forEach(item => {
+const digitButtons = document.querySelectorAll('.digits');
+const decimalButton = document.querySelector('#decimal');
+const displayInput = document.querySelector('#currentInput');
+const ACBUTTON = document.querySelector('#AC');
+const operandButtons = document.querySelectorAll('.operandButton');
+const history = document.querySelector('#history');
+let operation = '';
+let values = [];
+let result = 0; 
+
+//clear all entries
+ACBUTTON.addEventListener('click', allClear);
+function allClear() {
+    displayInput.innerHTML = '';
+    decimalButton.disabled = '';
+    history.innerHTML = '';
+    values = [];
+}
+
+digitButtons.forEach(item => {
     item.addEventListener('click', e => {
-        if(item.innerHTML === '.'){
-            console.log('you pressed decimal')
-            document.getElementById('currentInput').innerHTML += item.innerHTML;
-            document.getElementById('decimal').innerHTML = '';
+        if(item === decimalButton){
+           
+            decimalButton.disabled = 'true';
         }
-        document.getElementById('currentInput').innerHTML += item.innerHTML;
-    });
+        displayInput.innerHTML += item.innerHTML;
+    })
 });
 
 
+function clearEntry() {
+    displayInput.innerHTML = '';
+    decimalButton.disabled = '';
+}
 
-document.querySelectorAll('.operandButton').forEach(item => {
+operandButtons.forEach(item => {
     item.addEventListener('click', e => {
-        let operand = item.innerHTML;
-        let value = document.getElementById('currentInput').innerHTML;
         
-
-        document.getElementById('history').innerHTML += `${value} ${operand} `;
-        document.getElementById('currentInput').innerHTML = '';
-        console.log(operand)
+        operation = item.innerHTML;
+        values.push(displayInput.innerHTML);
+        history.innerHTML += ` ${displayInput.innerHTML} ${item.innerHTML}`
+       displayInput.innerHTML = '';
+       console.log(`${item.innerHTML} clicked`)
+       
     });
 });
+
+
+
