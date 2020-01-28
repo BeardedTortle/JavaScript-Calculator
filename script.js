0dc7dd074749d37f88f0378d9250bc25cdffd425
Kelly Cox <thekellycox@gmail.com>
2020-01-27 23:18:33 -0500

    getting there. need to add failsafes to stop user from breaking function. needs more polishing

--- script.js
+++ script.js
@@ -37,14 +37,63 @@ function clearEntry() {
 operandButtons.forEach(item => {
     item.addEventListener('click', e => {
         
+        if(item.innerHTML === '='){
+        values.push(parseInt(displayInput.innerHTML));
+        operate();
+        displayInput.innerHTML = `${result}`;
+        history.innerHTML = '';
+        values = [];
+        operation = '';
+        }else {
+            if(values.length < 2){
         operation = item.innerHTML;
-        values.push(displayInput.innerHTML);
+        values.push(parseInt(displayInput.innerHTML));
         history.innerHTML += ` ${displayInput.innerHTML} ${item.innerHTML}`
-       displayInput.innerHTML = '';
-       console.log(`${item.innerHTML} clicked`)
+        displayInput.innerHTML = '';
        
+        console.log(values)
+        console.log(values.length);} else {
+            operate();
+            console.log(values)
+            history.innerHTML = `${result} ${item.innerHTML}`;
+            displayInput.innerHTML = '';
+        }}
+
+        
     });
 });
 
+function operate(){
+switch (operation){
+    case '+':
+        result = addFunction();
+        values = [result, ]
+        break;
+    case '-':
+        result = subFunction();
+        values = [result, ]
+        break;
+    case '*':
+        result = multFunction();
+        values = [result, ]
+        break;
+    case '/':
+        result = divFunction();
+        values = [result, ]
+}}
+
+function addFunction(){ 
+    return values.reduce((a,b)=> a+b)
+}
+function subFunction(){ 
+    return values.reduce((a,b)=> a-b)
+}
+function multFunction(){ 
+    return values.reduce((a,b)=> a*b)
+}
+function divFunction(){ 
+    return values.reduce((a,b)=> a/b)
+}
 
 
+console.log(values)
