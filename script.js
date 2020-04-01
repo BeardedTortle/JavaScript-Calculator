d2d5b399e05badd36e2534b6aeeb8091a271c57c
Kelly Cox <thekellycox@gmail.com>
2020-04-01 01:07:23 -0400

    added listeners for operand buttons.

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ subDisplay.innerHTML = '0';
 const NUMS = document.getElementsByClassName('digits');
 const OPS = document.getElementsByClassName('operandButton');
 
-let value = ''
+let value = '';
+let operator = '';
+let valuesArr = [];
 
 function enterValue() {
     value = this.innerHTML;
+    console.log(value);
     if( value === '.'){
         document.querySelector('#dot').disabled = true;
     }
@@ -20,9 +23,32 @@ function enterValue() {
     display.innerHTML += value;
 }
 
+function operate(){
+    console.log('= test');
+}
+
+function selectOPS() {
+    operator = this.innerHTML;
+    console.log(operator);    
+}
+
 function numberListener(){
 for(let i = 0; i < NUMS.length; i++){
     NUMS[i].addEventListener('click', enterValue);
 }
 };
+function operandListener(){
+    for (let x = 0; x < OPS.length; x++){
+        OPS[x].addEventListener('click', selectOPS);
+    }
+}
 numberListener();
+operandListener();
+
+function clearEntry(){
+    display.innerHTML = '0';
+    value = '';
+}
+
+document.getElementById("clearInput").addEventListener('click', clearEntry);
+
